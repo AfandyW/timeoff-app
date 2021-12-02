9e8e6d51147f1cff3d6d5fec5fd589cdb0f401ce
afandy2000 <afandywibowo2000@gmail.com>
2021-12-02 15:19:54 +0800

    update sequelize config

---
+++ src/infra/db/mysql/database.ts
@@ -0,0 +1,28 @@
+require("dotenv").config()
+
+module.exports = {
+    development: {
+        username: process.env.DEVELOPMENT_DB_USERNAME,
+        password: process.env.DEVELOPMENT_DB_PASSWORD,
+        database: process.env.DEVELOPMENT_DB_DATABASE,
+        host: process.env.DEVELOPMENT_DB_HOST,
+        port: process.env.DEVELOPMENT_DB_PORT,
+        dialect: "mysql",
+    },
+    test: {
+        username: process.env.TEST_DB_USERNAME,
+        password: process.env.TEST_DB_PASSWORD,
+        database: process.env.TEST_DB_DATABASE,
+        host: process.env.TEST_DB_HOST,
+        port: process.env.TEST_DB_PORT,
+        dialect: "mysql",
+    },
+    production: {
+        username: process.env.PRODUCTION_DB_USERNAME,
+        password: process.env.PRODUCTION_DB_PASSWORD,
+        database: process.env.PRODUCTION_DB_DATABASE,
+        host: process.env.PRODUCTION_DB_HOST,
+        port: process.env.PRODUCTION_DB_PORT,
+        dialect: "mysql",
+    },
+}
