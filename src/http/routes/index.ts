86ea94c97d0141b475aca278d950122354a7802e
afandy2000 <afandywibowo2000@gmail.com>
2021-12-21 12:07:30 +0800

    new route

--- src/http/routes/index.ts
+++ src/http/routes/index.ts
@@ -12,6 +12,99 @@ router.get('/ping', async (req,res) => {
     res.json('message :pong')
 })
 
+router.get('/employees', async (req, res) => {
+    try{
+        const employees = await model.Employee.findAll()
+
+        const response: responses = {
+            code: 200,
+            status: "Success Get Data",
+            data: employees
+        }
+
+        res.status(200).json(response)
+    } catch{
+        res.status(500)
+        res.json({
+                "code": 500,
+                "status": "Internal Server Error"
+        })
+    }
+})
+
+router.post('/employees', async (req, res) => {
+    try{
+        const { position_id, name, phone, email } = req.body
+
+        await model.Employee.create({
+            position_id: position_id,
+            name: name,
+            phone: phone,
+            email: email
+        })
+
+        const response: responses = {
+            code: 201,
+            status: "Success Create New Employee",
+        }
+
+        res.status(201).json(response)
+    } catch(err){
+        console.log(err)
+        res.status(500)
+        res.json({
+                "code": 500,
+                "status": "Internal Server Error"
+        })
+    }
+})
+
+router.put('/employees/:id', async (req, res) => {
+    try{
+        const { position_id, name, phone, email, direct_report_employee } = req.body
+        const {id} = req.params
+
+        const employeeId = parseInt(id)
+
+        const employee = await model.Employee.findByPk(employeeId)
+
+        if (!employee) {
+            const err = new NotFoundError("Employee Not Found")
+            const response: responses = {
+                code: err.code,
+                status: err.message
+            }
+            res.status(404).json(response)
+            return
+        }
+
+        await model.Employee.update({
+            position_id: position_id,
+            name: name,
+            phone: phone,
+            email: email,
+            direct_report_employee: direct_report_employee
+        }, {
+            where: {
+                id: id
+            }
+        })
+
+        const response: responses = {
+            code: 201,
+            status: "Success Update data employee",
+        }
+
+        res.status(201).json(response)
+    } catch{
+        res.status(500)
+        res.json({
+                "code": 500,
+                "status": "Internal Server Error"
+        })
+    }
+})
+
 router.get('/position', async (req,res) => {
     try{
         const positions = await model.Position.findAll()
@@ -24,11 +117,11 @@ router.get('/position', async (req,res) => {
 
         res.status(200).json(response)
     } catch{
-        const response : responses = {
-            code : 200,
-            status: "Success Get Data",
-        }
-        res.status(500).json(response)
+        res.status(500)
+        res.json({
+                "code": 500,
+                "status": "Internal Server Error"
+        })
     }
 })
 
@@ -63,11 +156,11 @@ router.put('/position/:id', async (req,res) => {
 
         res.status(201).json(response)
     }catch{
-        res.status(500)
-        res.json({
-                "code": 500,
-                "status": "Internal Server Error"
-        })
+        const response : responses = {
+            code : 500,
+            status: "Internal Server Error"
+        }
+        res.status(500).json(response)
     }
 })
 
@@ -88,7 +181,7 @@ router.post('/position', async (req,res) => {
     }catch{
         const response : responses = {
             code : 500,
-            status: "Success Get Data",
+            status: "Internal Server Error"
         }
         res.status(500).json(response)
     }
